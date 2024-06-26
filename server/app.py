d6a6525f46d578b72a026c4058b4ecf3784ce119
gatere <mercynjerigatere@gmail.com>
2024-06-26 11:06:16 +0300

    Adds lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,39 @@
-#!/usr/bin/env python3
+
 
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def index ():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+
+@app.route("/print/<string>")
+def print_string(string):
+    print(string)
+    return string
+
+@app.route("/count/<integer>")
+def count (integer):
+    if  isinstance (integer, int):
+        return range(0,integer)
+    
+@app.route("/math/<num1>/<operation>/<num2")
+def math(num1, operation, num2):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1-num2
+    elif operation == "*":
+        return num1*num2
+    elif operation == "%":
+        return num1/num2
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
